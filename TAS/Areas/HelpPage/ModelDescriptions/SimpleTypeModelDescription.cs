2b5169655a15276d3f611b898dc0775370757f83
piotrowskislawomir <piotrowski.slawomir590@gmail.com>
2014-10-14 16:40:37 +0200

    comment

---
+++ TAS/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TAS.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TAS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TAS.WebApiApplication" Language="C#" %>
