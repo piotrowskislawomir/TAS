f143de06a62254af04313dde253ba7b67a3cf0f0
piotrowskislawomir <piotrowski.slawomir590@gmail.com>
2014-10-19 16:21:23 +0200

    Branch

--- TAS/Global.asax.cs
+++ TAS/Global.asax.cs
@@ -16,7 +16,7 @@ namespace TAS
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RouteConfig.RegisterRoutes(RouteTable.Routes); 
         }
     }
 }
