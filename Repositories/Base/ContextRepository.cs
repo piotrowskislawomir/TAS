63423f26011fdc6e67c70c791f431ac8422e86db
piotrowskislawomir <piotrowski.slawomir590@gmail.com>
2014-10-28 14:48:59 +0100

    mmm

--- Repositories/Base/ContextRepository.cs
+++ Repositories/Base/ContextRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Models
+namespace Repositories.Base
 {
-    public class Class1
+    public class ContextRepository : BaseRepository<Models.Db.TASEntities>
     {
     }
 }
